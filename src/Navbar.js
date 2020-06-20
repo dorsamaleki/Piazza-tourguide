c93a40512d95d75328811bee1ea527210cce9d28
Dorsa Maleki <a.malekidorsa@gmail.com>
2020-06-20 11:37:33 +0430

    cool navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,22 +5,25 @@ export const Navbar = () => {
   return (
     <div className={styles.navbar}>
       <img
+        className={styles.logo}
         src="https://piazza.com/images/splash/PageTop/Piazza-Icon.png"
         alt="logo"
       />
-      <nav className={styles.nav}>
-        <li>
-          <a href="https://www.google.com/">Resources</a>
-        </li>
-        <li>
-          <a href="https://www.google.com/">Statistics</a>
-        </li>
-        <li>
-          <a href="https://www.google.com/">Home</a>
-        </li>
-        <li>
-          <a href="https://www.google.com/">Cs 154</a>
-        </li>
+      <nav>
+        <ul>
+          <li>
+            <a href="https://www.google.com/">Ce 87</a>
+          </li>
+          <li>
+            <a href="https://www.google.com/">Q&A</a>
+          </li>
+          <li>
+            <a href="https://www.google.com/">Resources</a>
+          </li>
+          <li>
+            <a href="https://www.google.com/">Statistics</a>
+          </li>
+        </ul>
       </nav>
       <div></div>
     </div>
