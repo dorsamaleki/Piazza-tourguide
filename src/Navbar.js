54784fbf10d1a7dd19f6a4d58316ced10236def4
Dorsa Maleki <a.malekidorsa@gmail.com>
2020-06-20 17:13:10 +0430

    multiple fixed top navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,29 +3,66 @@ import styles from "./Navbar.module.css";
 
 export const Navbar = () => {
   return (
-    <div className={styles.navbar}>
-      <img
-        className={styles.logo}
-        src="https://piazza.com/images/splash/PageTop/Piazza-Icon.png"
-        alt="logo"
-      />
-      <nav>
-        <ul>
-          <li>
-            <a href="https://www.google.com/">Ce 87</a>
-          </li>
-          <li>
-            <a href="https://www.google.com/">Q&A</a>
-          </li>
-          <li>
-            <a href="https://www.google.com/">Resources</a>
-          </li>
-          <li>
-            <a href="https://www.google.com/">Statistics</a>
-          </li>
-        </ul>
-      </nav>
-      <div></div>
+    <div>
+      <div className={styles.navbar1}>
+        <img
+          src="https://piazza.com/images/splash/PageTop/Piazza-Icon.png"
+          alt="logo"
+        />
+        <nav>
+          <ul>
+            <li>
+              <a href="https://www.google.com/">Ce 87</a>
+            </li>
+            <li>
+              <a href="https://www.google.com/">Q&A</a>
+            </li>
+            <li>
+              <a href="https://www.google.com/">Resources</a>
+            </li>
+            <li>
+              <a href="https://www.google.com/">Statistics</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className={styles.navbar2}>
+        <img
+          src="https://cdn.clipart.email/9ab111a3d83e524f23e2fad7d5345d7a_folder-replacement-yellow-folder-icon-transparent-background-png-_512-512.jpeg"
+          alt="logo"
+        />
+        <img
+          src="https://cdn.clipart.email/9ab111a3d83e524f23e2fad7d5345d7a_folder-replacement-yellow-folder-icon-transparent-background-png-_512-512.jpeg"
+          alt="logo"
+        />
+        <img
+          src="https://cdn.clipart.email/9ab111a3d83e524f23e2fad7d5345d7a_folder-replacement-yellow-folder-icon-transparent-background-png-_512-512.jpeg"
+          alt="logo"
+        />
+        <img
+          src="https://cdn.clipart.email/9ab111a3d83e524f23e2fad7d5345d7a_folder-replacement-yellow-folder-icon-transparent-background-png-_512-512.jpeg"
+          alt="logo"
+        />
+        <img
+          src="https://cdn.clipart.email/9ab111a3d83e524f23e2fad7d5345d7a_folder-replacement-yellow-folder-icon-transparent-background-png-_512-512.jpeg"
+          alt="logo"
+        />
+        <img
+          src="https://cdn.clipart.email/9ab111a3d83e524f23e2fad7d5345d7a_folder-replacement-yellow-folder-icon-transparent-background-png-_512-512.jpeg"
+          alt="logo"
+        />
+        <img
+          src="https://cdn.clipart.email/9ab111a3d83e524f23e2fad7d5345d7a_folder-replacement-yellow-folder-icon-transparent-background-png-_512-512.jpeg"
+          alt="logo"
+        />
+        <nav>
+          <ul>
+            <li>
+              <a href="https://www.google.com/">Drafts</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 };
