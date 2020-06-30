1f5c8c922359a5c8684f81c1a9b06fec4934f973
Dorsa Maleki <a.malekidorsa@gmail.com>
2020-06-30 12:30:15 +0430

    side bar

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,14 +1,38 @@
 import React from "react";
 import styles from "./SideBar.module.css";
 
-const eee = ["Question 1", "Question 2", "Question 3", "Question 4"];
+const eee = [
+  "Question 1",
+  "Question 2",
+  "Question 3",
+  "Question 4",
+  "Question 1",
+  "Question 2",
+  "Question 3",
+  "Question 4",
+];
 
 export const SideBar = (props) => {
   return (
-    <div className={styles.root}>
-      {eee.map((content, index) => {
-        return <div>{content}</div>;
-      })}
+    <div>
+      <div className={styles.a}>
+        <div className={styles.j}>
+          <nav>
+            <ul>
+              <li>
+                <a href="https://www.google.com/">Ce 87</a>
+
+                <a href="https://www.google.com/">Ce 87</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className={styles.c}>
+          {eee.map((content, index) => {
+            return <div className={styles.b}>{content}</div>;
+          })}
+        </div>
+      </div>
     </div>
   );
 };
