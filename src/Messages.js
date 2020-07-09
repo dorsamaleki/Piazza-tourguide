453ee24470d935133a456f9d453d14f10161d2ca
Dorsa Maleki <a.malekidorsa@gmail.com>
2020-07-09 11:09:53 +0430

    layer

--- src/Messages.js
+++ src/Messages.js
@@ -4,9 +4,13 @@ import styles from "./Messages.module.css";
 export const Messages = (props) => {
   return (
     <div className={styles.root}>
-      {props.list.map((question) => {
-        return <div>{question.text}</div>;
-      })}
+      <div>PnllnlTYPE</div>
+
+      <div className={styles.cover}>
+        {props.list.map((question) => {
+          return <div className={styles.k}>{question.text}</div>;
+        })}
+      </div>
     </div>
   );
 };
