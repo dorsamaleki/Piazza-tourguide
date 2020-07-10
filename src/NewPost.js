a33b4221dc5a45eaef63fb36da31a51e1815c87b
Dorsa Maleki <a.malekidorsa@gmail.com>
2020-07-10 12:58:40 +0430

    radio button in New post

--- src/NewPost.js
+++ src/NewPost.js
@@ -1,5 +1,23 @@
 import React from "react";
 
 export const NewPost = (props) => {
-  return <div>bhkb</div>;
+  return (
+    <div>
+      <form>
+        <label>
+          <input type="radio" value="option1" checked={true} />
+          Question
+        </label>
+        <label>
+          <input type="radio" value="option1" checked={true} />
+          Poll
+        </label>
+        <label>
+          <input type="radio" value="option1" checked={true} />
+          Note
+        </label>
+      </form>
+      <div>hhooo</div>
+    </div>
+  );
 };
